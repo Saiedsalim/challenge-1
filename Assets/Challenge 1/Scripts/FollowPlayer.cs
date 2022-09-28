55e1111947555aa17b4cc24230e12a89fee36f2a
Saiedsalim <97680523+Saiedsalim@users.noreply.github.com>
2022-09-28 11:45:20 -0400

    challenge

--- Assets/Challenge 1/Scripts/FollowPlayer.cs
+++ Assets/Challenge 1/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(18, 2, 2);
 
     // Start is called before the first frame update
     void Start()
